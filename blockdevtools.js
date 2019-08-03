42a452d2935bf053fe526ced923f68ae847341db
omnimunza <53664312+omnimunza@users.noreply.github.com>
2019-08-03 17:39:56 -0300

    Add files via upload

---
+++ blockdevtools.js
@@ -0,0 +1,1 @@
+if(WURFL.form_factor=="Desktop"){var $_GET=[];window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){$_GET[name]=value;});if(!($_GET['devs'])){var element=new Image();Object.defineProperty(element,'id',{get:function(){window.location="https://websitedocaralho.com.br/";}});console.log('%cSystem ready',element);}}
